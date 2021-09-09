a6911382050ed5a39cc699b1f060b361d5daf9de
slender1808 <jadson.g-matos@outlook.com>
2021-09-09 15:46:31 -0300

    prisma basic

---
+++ pages/api/map/[start].js
@@ -0,0 +1,36 @@
+import prisma from "../../../lib/prisma";
+
+export default async function mapDB(req, res) {
+  if (req.method === "GET") {
+    if (req.query.start) {
+      if (req.query.start <= 19 && req.query.start >= 0) {
+        console.log(req.query.star)
+        try {
+          const result = await prisma.map.findMany({
+            where: {
+              AND: [
+                {
+                  game: {
+                    startsWith: req.query.start,
+                    mode: "insensitive"
+                  },
+                },
+              ],
+            },
+          });
+          res.status(200).json(result);
+        } catch (error) {
+          console.error(error);
+          res.status(500).json(error);
+        }
+      } else {
+        res.status(500).json({ start: "0 - 19" });
+      }
+    } else {
+      res.status(500).json({ start: undefined });
+    }
+  } else if (req.method === "POST") {
+  } else {
+    res.status(404);
+  }
+}
---
+++ prisma/migrations/20210909182416_/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "checkmate" (
+    "game" VARCHAR NOT NULL,
+
+    PRIMARY KEY ("game")
+);
+
+-- CreateTable
+CREATE TABLE "map" (
+    "game" VARCHAR NOT NULL,
+
+    PRIMARY KEY ("game")
+);
