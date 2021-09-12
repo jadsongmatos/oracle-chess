6977c706556dd723382fd918f9e9ac77da29a54d
slender1808 <jadson.g-matos@outlook.com>
2021-09-12 01:41:57 -0300

    0.0.2

--- pages/api/progress.js
+++ pages/api/progress.js
@@ -1,10 +1,14 @@
-import getProgress from "../../../lib/getProgress";
+import getProgress from "../../lib/getProgress";
+import postProgress from "../../lib/postProgress";
 
-export default async function mapDB(req, res) {
+export default async function progress(req, res) {
   if (req.method === "GET") {
     const result = await getProgress();
     res.status(result.status).json(result.data);
   } else if (req.method === "POST") {
+    console.log(req.body)
+    const result = await postProgress(req.body);
+    res.status(result.status).json(result.data);
   } else {
     res.status(404).json({ method: ["GET", "POST"] });
   }
